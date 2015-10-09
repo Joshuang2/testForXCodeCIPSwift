79093535064693c28b277b4aa63ef74df0d274a5
joshuang2 <joshuang2@datacom.co.nz>
2015-10-09 14:49:08 +1300

    Added a press me button which fade transitions to a hello world label.

--- simpleTestProjectiOS/ViewController.swift
+++ simpleTestProjectiOS/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var pressMeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pressMeButton.setTitle("Press Me", forState: UIControlState.Normal)
+        aLabel.alpha = 0
         aLabel.text = "Hello World"
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -24,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onPressed(sender: UIButton) {
+        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { () -> Void in
+            sender.alpha = 0
+            }) { (success) -> Void in
+                if success {
+                    UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { () -> Void in
+                        self.aLabel.alpha = 1
+                        }, completion: nil)
+                }
+        }
+    }
 
 }
 
